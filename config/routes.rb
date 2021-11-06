ef2c6856312f7a850d1c90f735b6dd84ac1e68fa
EC2 Default User <ec2-user@ip-172-31-39-206.ap-northeast-1.compute.internal>
2021-11-06 14:09:19 +0000

    c

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'questions#index'
-  get 'questions/index'
+  resources :questions, only: [:index, :new, :create]
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
