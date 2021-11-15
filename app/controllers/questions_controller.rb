ac088fa3fef157395e91d9a3f031dbac8ac8400d
EC2 Default User <ec2-user@ip-172-31-39-206.ap-northeast-1.compute.internal>
2021-11-15 13:39:05 +0000

    1115

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,12 @@
 class QuestionsController < ApplicationController
   def index
   end
+  
+  def create 
+    @question = Question.create!(question: params[:question], user_id: current_user.id)
+    Option.create!(option: params[:option1], user_id: current_user.id, question_id: @question.id)
+    Option.create!(option: params[:option2], user_id: current_user.id, question_id: @question.id)
+    Option.create!(option: params[:option3], user_id: current_user.id, question_id: @question.id)
+    redirect_to "/questions"
+  end
 end
