f935a8cf5e4a8ae50da64914b91d14056d02ce12
EC2 Default User <ec2-user@ip-172-31-39-206.ap-northeast-1.compute.internal>
2021-11-22 14:08:32 +0000

    index cont

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,14 @@
 class QuestionsController < ApplicationController
+  before_action :authenticate_user!
+  
   def index
     @questions = Question.all.order(created_at: :desc)
+    
+    user_options = UserOption.where(user_id: current_user.id)
+    
+    options = Option.where(question_id: @questions.pluck(:id), id: user_options.pluck(:option_id))
+    
+    @answered_question_ids = options.pluck(:question_id)
   end
   
   def create 
